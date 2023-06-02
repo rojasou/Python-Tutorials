7bf5e98e5176596acac8f3bf9e9a174180d7d16c
rojasou <135290503+rojasou@users.noreply.github.com>
2023-06-01 23:32:50 -0400

    Add files via upload

---
+++ Basic Python.py
@@ -0,0 +1,17 @@
+from math import sqrt
+from math import factorial
+print("Hello World!")
+# This is assigning the variables x and y
+x = 123 * 4
+y = 500
+# This will print the variable x
+print(x)
+# This will print the variable y
+print(y)
+
+# More complex math
+number = 9
+# Using math functions need the math package
+print(sqrt(number))
+# For every function you must import from math package
+print(factorial(number))
