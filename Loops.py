9a240fde83d7b9b72f27cbe6f147199c43b0f2ae
rojasou <135290503+rojasou@users.noreply.github.com>
2023-06-03 23:56:25 -0400

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,13 @@
+# create a list
+my_list = [9, 5, 3, 6, 12]
+# let's add 5 to each element to the list using a 'for' loop
+for element in my_list:
+    element = element + 5
+    print(element)
+
+# 'if' loop
+a = 11
+if a % 2 == 0:
+    print("a is divisible by 2")
+else:
+    print("a is not divisible by 2")
