71edaf008b4df56accfe4a305cbc221c492d6895
rojasou <135290503+rojasou@users.noreply.github.com>
2023-06-06 18:13:03 -0400

    Add files via upload

---
+++ YTmp3.py
@@ -0,0 +1,23 @@
+# importing packages
+from pytube import YouTube
+import os
+
+# url input from user
+yt = YouTube(input("Enter the URL of the video you want to download: \n>> "))
+
+# extract only audio
+video = yt.streams.filter(only_audio=True).first()
+
+# replace destination with the path where you want to save the downloaded file
+destination = r"C:\Users\bryan\Music\Music"
+
+# download the file
+out_file = video.download(output_path=destination)
+
+# save the file
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+
+# result of success
+print(yt.title + " has been successfully downloaded.")
